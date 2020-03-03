d7a6aab90598beeda91e276d6fd920af81b6983e
Param Bole <pbole@usc.edu>
2020-03-02 23:47:41 -0800

    - First commit - Topic of the day trees

---
+++ Trees/112-Path-Sum.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        
+        
+        //SEE TUSHAR ROY VIDEO TO UNDERSTAND
+        
+        
+        if(root == null) return false;
+        
+        sum -= root.val;
+        if(root.left == null && root.right == null) 
+            return (sum == 0);
+        
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
+}
