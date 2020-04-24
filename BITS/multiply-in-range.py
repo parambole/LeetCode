18b7da41cd7e29961c39aa5448dd93479817bfa3
Param Bole <pbole@usc.edu>
2020-04-23 19:45:05 -0700

    Day 23 - Do again

---
+++ BITS/multiply-in-range.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rangeBitwiseAnd(self, m: int, n: int) -> int:
+        
+        # understand in a better way why does the common prefix not matter
+        
+        shift = 0
+        while (m != n):
+            
+            m = m >> 1
+            n = n >> 1
+            shift += 1
+            
+        return m << shift
