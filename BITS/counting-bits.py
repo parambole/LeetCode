a4fb3d177e9544ad71a7188a90021956e1ff4ff5
Param Bole <pbole@usc.edu>
2020-05-28 19:10:16 -0700

    May 28 LC challenge

---
+++ BITS/counting-bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        dp = [0] * (num+1)
+        
+        for i in range(1, num+1):
+            if i % 2 != 0:
+                
+                dp[i] = dp[i//2] + 1
+            else:
+                dp[i] = dp[i//2]
+        return dp
