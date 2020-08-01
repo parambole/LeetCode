5ef15862e8f2628b82f84075f97fab30ef7920b4
Param Bole <pbole@usc.edu>
2020-08-01 11:05:01 -0700

    Aug 1 LC

---
+++ Design/logger-rate.py
@@ -0,0 +1,29 @@
+class Logger:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.dic = {}
+        
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity.
+        """
+        if message not in self.dic:
+            self.dic[message] = timestamp
+            return True
+        else:
+            if timestamp - self.dic[message] >= 10:
+                self.dic[message] = timestamp
+                return True
+            else:
+                return False
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
