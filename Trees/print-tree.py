c3016dcdfac6118fdef7a6b4677ca5452b7df73b
Param Bole <pbole@usc.edu>
2021-01-06 12:33:27 +0530

    Uber FT

---
+++ Trees/print-tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def printTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[str]]
+        """
+        def get_height(node):
+            if not node:
+                return 0
+            return 1 + max(get_height(node.left), get_height(node.right))
+
+        rows = get_height(root)
+        cols = 2 ** rows - 1
+        res = [['' for _ in range(cols)] for _ in range(rows)]
+
+        def traverse(node, level, pos):
+            if not node:
+                return
+            left_padding, spacing = 2 ** (rows - level - 1) - 1, 2 ** (rows - level) - 1
+            index = left_padding + pos * (spacing + 1)
+            print(level, index, node.val)
+            res[level][index] = str(node.val)
+            traverse(node.left, level + 1, pos << 1)
+            traverse(node.right, level + 1, (pos << 1) + 1)
+        traverse(root, 0, 0)
+        return res
