cfe14aa905641699587b65010f666eef721f92e2
Param Bole <pbole@usc.edu>
2021-01-19 05:52:42 +0530

    U FT union find

---
+++ Graph/evaluate-division.py
@@ -0,0 +1,48 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+
+        gid_weight = {}
+
+        def find(node_id):
+            if node_id not in gid_weight:
+                gid_weight[node_id] = (node_id, 1)
+            group_id, node_weight = gid_weight[node_id]
+            # The above statements are equivalent to the following one
+            #group_id, node_weight = gid_weight.setdefault(node_id, (node_id, 1))
+
+            if group_id != node_id:
+                # found inconsistency, trigger chain update
+                new_group_id, group_weight = find(group_id)
+                gid_weight[node_id] = \
+                    (new_group_id, node_weight * group_weight)
+            return gid_weight[node_id]
+
+        def union(dividend, divisor, value):
+            dividend_gid, dividend_weight = find(dividend)
+            divisor_gid, divisor_weight = find(divisor)
+            if dividend_gid != divisor_gid:
+                # merge the two groups together,
+                # by attaching the dividend group to the one of divisor
+                gid_weight[dividend_gid] = \
+                    (divisor_gid, divisor_weight * value / dividend_weight)
+
+        # Step 1). build the union groups
+        for (dividend, divisor), value in zip(equations, values):
+            union(dividend, divisor, value)
+
+        results = []
+        # Step 2). run the evaluation, with "lazy" updates in find() function
+        for (dividend, divisor) in queries:
+            if dividend not in gid_weight or divisor not in gid_weight:
+                # case 1). at least one variable did not appear before
+                results.append(-1.0)
+            else:
+                dividend_gid, dividend_weight = find(dividend)
+                divisor_gid, divisor_weight = find(divisor)
+                if dividend_gid != divisor_gid:
+                    # case 2). the variables do not belong to the same chain/group
+                    results.append(-1.0)
+                else:
+                    # case 3). there is a chain/path between the variables
+                    results.append(dividend_weight / divisor_weight)
+        return results
