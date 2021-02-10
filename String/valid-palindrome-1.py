ad3b4baaa73b5dec75831df07bb0db420f28dacb
Param Bole <pbole@usc.edu>
2021-02-10 22:54:04 +0530

    FE5

---
+++ String/valid-palindrome-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+        while ( i < j ):
+            
+            
+            # Remeber these two conditions also
+            while i < j and not s[i].isalnum():
+                i += 1
+                
+            while j > i and not s[j].isalnum():
+                j -= 1
+                
+            if s[i].lower() != s[j].lower():
+                return False
+            
+            # Imp now you need to more the character right if they match
+            
+            i += 1
+            j -= 1
+            
+        return True
